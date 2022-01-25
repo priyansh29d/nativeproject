8cc3f8c89f7b4a3b95436b23c58b85ae3d12221b
priyansh29d <priyash929@gmail.com>
2022-01-25 15:04:25 +0530

    Commit

--- App.js
+++ App.js
@@ -1,42 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import SectionListBasics from './components/SectionListBasics';
+import FlatListBasics from './components/FlatListBasics';
 
 export default function App() {
 
-  const [people, setPeople] = useState(
-    [
-      { name: 'Priyansh', key: '1' },
-      { name: 'Jain', key: '2' },
-      { name: 'Avichal', key: '3' },
-      { name: 'Pathak', key: '4' },
-      { name: 'Divyang', key: '5' },
-      { name: 'Sharma', key: '6' },
-      { name: 'Trainer1', key: '7' },
-      { name: 'Trainer2', key: '8' },
-      { name: 'Trainer3', key: '9' },
-      { name: 'Trainer4', key: '10' },
-      { name: 'Trainer5', key: '11' },
-      { name: 'Trainer6', key: '12' },
-      { name: 'Trainer7', key: '13' },
-      { name: 'Trainer8', key: '14' },
-      { name: 'Trainer9', key: '15' },
-    ]
-  )
+
 
   return (
     <View style={styles.container}>
-      <ScrollView>
-        {
-          people.map((item) => {
-            return (
-              <View key={item.key}>
-                <Text style={styles.item}>{item.name}</Text>
-              </View>
-            )
-          }
-          )}
-      </ScrollView>
+      <SectionListBasics />
+      <FlatListBasics />
 
     </View>
 
@@ -64,7 +39,7 @@ const styles = StyleSheet.create({
     marginTop: 24,
     padding: 30,
     height: 30,
-    backgroundColor: 'yellow',
+    backgroundColor: 'pink',
     fontSize: 24,
   }
 
